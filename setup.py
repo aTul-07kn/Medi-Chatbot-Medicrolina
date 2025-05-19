860a8a73a7c69eb717090f3f4b9fbb569ff65a86
aTul-07kn <atulnayak7869@gmail.com>
2025-05-19 17:32:27 +0530

    Project structure created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medicrolina Chatbot',
+    version= '0.0.0',
+    author= 'Atul Kumar Nayak',
+    author_email= 'atulnayak7869@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
